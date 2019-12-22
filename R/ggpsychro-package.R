8fbbc253db19803f376e84b4de5eeb8bc1e3bba0
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2019-12-23 07:51:52 +0800

    Depends on ggplot2.

--- R/ggpsychro-package.R
+++ R/ggpsychro-package.R
@@ -4,6 +4,5 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @import ggplot2
 ## usethis namespace: end
 NULL
