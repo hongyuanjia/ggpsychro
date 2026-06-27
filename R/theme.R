75c6a7a5b56df67641b1e3d605853b92127dfbc8
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2026-06-28 04:24:22 +0800

    feat: add psychrometric chart presets
    * feat: add psychrometric chart presets
    
    * fix: densify ashrae chart preset
    
    * fix: align ashrae preset with psychrochart style
    
    * fix: align minimal preset with psychrochart style

--- R/theme.R
+++ R/theme.R
@@ -3,6 +3,13 @@
 #' @inheritParams ggplot2::theme_grey
 #' @return A ggplot2 theme.
 #'
+#' `theme_psychro_ashrae()` and `theme_psychro_minimal()` are inspired by
+#' psychrochart's ASHRAE and minimal chart styles.
+#'
+#' @seealso
+#' * [psychrochart ASHRAE style](https://github.com/azogue/psychrochart/blob/master/psychrochart/chart_styles/ashrae_chart_style.json)
+#' * [psychrochart minimal style](https://github.com/azogue/psychrochart/blob/master/psychrochart/chart_styles/minimal_chart_style.json)
+#'
 #' @author Hongyuan Jia
 #' @importFrom ggplot2 "%+replace%"
 #' @examples
@@ -100,32 +107,35 @@ theme_psychro_minimal <- function(base_size = 11, base_family = "",
     ggplot2::theme(
          panel.border = ggplot2::element_blank(),
          panel.background = ggplot2::element_blank(),
-         axis.line.x = ggplot2::element_line(color = "gray25", linewidth = 0.35),
-         axis.line.y = ggplot2::element_line(color = "gray25", linewidth = 0.35),
-         axis.ticks = ggplot2::element_line(color = "gray35", linewidth = 0.25),
-         axis.text = ggplot2::element_text(color = "gray25"),
-         axis.title = ggplot2::element_text(color = "gray20"),
+         axis.line.x = ggplot2::element_line(color = grDevices::rgb(0.855, 0.145, 0.114), linewidth = 0.75),
+         axis.line.y = ggplot2::element_line(color = grDevices::rgb(0, 0.125, 0.376), linewidth = 0.75),
+         axis.ticks.x = ggplot2::element_line(color = grDevices::rgb(0.855, 0.145, 0.114), linewidth = 0.35),
+         axis.ticks.y = ggplot2::element_line(color = grDevices::rgb(0, 0.125, 0.376), linewidth = 0.35),
+         axis.text.x = ggplot2::element_text(color = grDevices::rgb(0.855, 0.145, 0.114)),
+         axis.text.y = ggplot2::element_text(color = grDevices::rgb(0, 0.125, 0.376)),
+         axis.title.x = ggplot2::element_text(color = grDevices::rgb(0.855, 0.145, 0.114)),
+         axis.title.y = ggplot2::element_text(color = grDevices::rgb(0, 0.125, 0.376)),
          psychro.panel.background = element_polygon(fill = "white", color = NA),
          psychro.panel.mask = element_polygon(fill = "white", color = NA),
-         panel.grid.major.x = ggplot2::element_line(color = "gray75", linetype = 1, linewidth = 0.18),
-         panel.grid.minor.x = ggplot2::element_line(color = "gray90", linetype = 1, linewidth = 0.12),
-         panel.grid.major.y = ggplot2::element_line(color = "gray75", linetype = 1, linewidth = 0.18),
-         panel.grid.minor.y = ggplot2::element_line(color = "gray90", linetype = 1, linewidth = 0.12),
-         psychro.panel.grid.saturation = ggplot2::element_line(color = "gray15", linewidth = 0.65),
-         psychro.panel.grid.relhum = ggplot2::element_line(color = "gray45", linewidth = 0.25, linetype = "solid"),
-         psychro.panel.grid.major.relhum = ggplot2::element_line(color = "gray35", linewidth = 0.3, linetype = "solid"),
-         psychro.panel.grid.minor.relhum = ggplot2::element_line(color = "gray85", linewidth = 0.12, linetype = "solid"),
-         psychro.panel.grid.wetbulb = ggplot2::element_line(color = "gray78", linewidth = 0.16, linetype = "dashed"),
-         psychro.panel.grid.major.wetbulb = ggplot2::element_line(color = "gray68", linewidth = 0.18, linetype = "dashed"),
-         psychro.panel.grid.minor.wetbulb = ggplot2::element_line(color = "gray90", linewidth = 0.1, linetype = "dashed"),
+         panel.grid.major.x = ggplot2::element_line(color = grDevices::rgb(0.855, 0.145, 0.114), linetype = "dotted", linewidth = 0.20),
+         panel.grid.minor.x = ggplot2::element_blank(),
+         panel.grid.major.y = ggplot2::element_blank(),
+         panel.grid.minor.y = ggplot2::element_blank(),
+         psychro.panel.grid.saturation = ggplot2::element_line(color = grDevices::rgb(0.855, 0.145, 0.114), linewidth = 1.4),
+         psychro.panel.grid.relhum = ggplot2::element_line(color = grDevices::rgb(0, 0.498, 1), linewidth = 0.5, linetype = "solid"),
+         psychro.panel.grid.major.relhum = ggplot2::element_line(color = grDevices::rgb(0, 0.498, 1), linewidth = 0.5, linetype = "solid"),
+         psychro.panel.grid.minor.relhum = ggplot2::element_blank(),
+         psychro.panel.grid.wetbulb = ggplot2::element_line(color = grDevices::rgb(0.498, 0.875, 1), linewidth = 0.25, linetype = "solid"),
+         psychro.panel.grid.major.wetbulb = ggplot2::element_line(color = grDevices::rgb(0.498, 0.875, 1), linewidth = 0.25, linetype = "solid"),
+         psychro.panel.grid.minor.wetbulb = ggplot2::element_line(color = grDevices::rgb(0.498, 0.875, 1), linewidth = 0.20, linetype = "solid"),
          psychro.panel.grid.vappres = ggplot2::element_blank(),
          psychro.panel.grid.major.vappres = ggplot2::element_blank(),
          psychro.panel.grid.minor.vappres = ggplot2::element_blank(),
-         psychro.panel.grid.specvol = ggplot2::element_blank(),
-         psychro.panel.grid.major.specvol = ggplot2::element_blank(),
+         psychro.panel.grid.specvol = ggplot2::element_line(color = grDevices::rgb(0, 0.502, 0.337), linewidth = 0.5, linetype = "solid"),
+         psychro.panel.grid.major.specvol = ggplot2::element_line(color = grDevices::rgb(0, 0.502, 0.337), linewidth = 0.5, linetype = "solid"),
          psychro.panel.grid.minor.specvol = ggplot2::element_blank(),
-         psychro.panel.grid.enthalpy = ggplot2::element_blank(),
-         psychro.panel.grid.major.enthalpy = ggplot2::element_blank(),
+         psychro.panel.grid.enthalpy = ggplot2::element_line(color = grDevices::rgb(0.251, 0, 0.502), linewidth = 0.5, linetype = "solid"),
+         psychro.panel.grid.major.enthalpy = ggplot2::element_line(color = grDevices::rgb(0.251, 0, 0.502), linewidth = 0.5, linetype = "solid"),
          psychro.panel.grid.minor.enthalpy = ggplot2::element_blank()
     )
 }
@@ -134,7 +144,8 @@ theme_psychro_minimal <- function(base_size = 11, base_family = "",
 #'
 #' `psychro_preset()` returns a list of ggplot additions that can be added to a
 #' [ggpsychro()] plot. Presets make selected reference grids explicit, configure
-#' grid labels, and apply a matching psychrometric chart theme.
+#' grid labels, and apply a matching psychrometric chart theme. The `"ashrae"`
+#' and `"minimal"` presets are inspired by psychrochart's chart styles.
 #'
 #' @param name A preset name. One of `"ashrae"` or `"minimal"`.
 #' @param labels A single logical value. If `FALSE`, preset grid labels are
@@ -191,11 +202,33 @@ psychro_preset <- function(name = c("ashrae", "minimal"), labels = TRUE) {
             theme_psychro_ashrae()
         ),
         minimal = list(
-            geom_grid_relhum(label = labels, label.size = 3.0),
-            geom_grid_wetbulb(label = FALSE),
+            scale_drybulb_continuous(
+                breaks = psychro_regular_breaks(5),
+                minor_breaks = NULL
+            ),
+            scale_relhum_continuous(
+                breaks = seq(20, 80, by = 20),
+                minor_breaks = NULL
+            ),
+            scale_wetbulb_continuous(
+                breaks = seq(10, 30, by = 10),
+                minor_breaks = seq(-10, 35, by = 5)
+            ),
+            scale_specvol_continuous(
+                limits = c(0.86, 0.98),
+                breaks = seq(0.86, 0.98, by = 0.04),
+                minor_breaks = NULL
+            ),
+            scale_enthalpy_continuous(
+                limits = c(20000, 120000),
+                breaks = seq(20000, 100000, by = 40000),
+                minor_breaks = NULL
+            ),
+            geom_grid_relhum(label = FALSE),
+            geom_grid_wetbulb(label = labels, label_loc = 0.22, label.size = 2.7),
             geom_grid_vappres(show = FALSE),
-            geom_grid_specvol(show = FALSE),
-            geom_grid_enthalpy(show = FALSE),
+            geom_grid_specvol(label = FALSE),
+            geom_grid_enthalpy(label = FALSE),
             theme_psychro_minimal()
         )
     )
